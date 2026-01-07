50480818c77b518dbc6cf9445c930ebbda488bd2
PedroBarrera <pedrodbarrerafdez@gmail.com>
2026-01-07 13:01:02 +0000

    CRUD de jedi lista, arreglar para mejor show

---
+++ index.js
@@ -0,0 +1,141 @@
+const URL = "http://localhost:3030";
+const jedisList = document.getElementById('jedisList');
+const jediForm = document.getElementById('jediForm');
+const jediName = document.getElementById('jediName');
+const jediSpecie = document.getElementById('jediSpecie');
+
+async function loadJedis() {
+    try {
+        jedisList.textContent = "";
+        let response = await fetch(`${URL}/jedis`);
+        if (response.ok) {
+            let jedis = await response.json();
+            //console.log(jedis);
+            jedis.forEach(jedi => {
+                insertJedi(jedi)
+            });
+        } else {
+            throw new Error('Fallo al cargar los datos')
+        }
+    } catch (error) {
+        console.log("ERROR: ", error)
+    }
+}
+
+function insertJedi(jedi) {
+    const li = document.createElement('li');
+    li.textContent = jedi.name;
+    const buttonDelete = document.createElement('button');
+    buttonDelete.textContent = "Borrar";
+    buttonDelete.classList.add('delete');
+    buttonDelete.dataset.id = jedi.id;
+    li.append(buttonDelete);
+    const buttonEdit = document.createElement('button');
+    buttonEdit.textContent = "Editar";
+    buttonEdit.classList.add('edit');
+    buttonEdit.dataset.id = jedi.id;
+    li.append(buttonEdit);
+    const buttonView = document.createElement('button');
+    buttonView.textContent = "Mostrar";
+    buttonView.classList.add('show');
+    buttonView.dataset.id = jedi.id;
+    li.append(buttonView);
+    jedisList.append(li);
+}
+
+jedisList.addEventListener('click', (event) => {
+    if (event.target.classList.contains('delete')) {
+        deleteJedi(event.target.dataset.id);
+    };
+    if (event.target.classList.contains('edit')) {
+        editJedi(event.target.dataset.id);
+    };
+    if (event.target.classList.contains('show')) {
+        showJedi(event.target.dataset.id);
+    };
+})
+
+async function deleteJedi(id) {
+    try {
+        let response = await fetch(`${URL}/jedis/${id}`, {
+            method: "delete"
+        });
+        if (response.ok) {
+            console.log('Elemento Eliminado');
+            loadJedis();
+        } else {
+            throw new Error('Fallo al eliminar')
+        }
+    } catch (error) {
+        console.log("ERROR: ", error)
+    }
+}
+
+jediForm.addEventListener('submit', async (event) => {
+    event.preventDefault();
+    let data = {
+        name: jediName.value,
+        species: jediSpecie.value
+    }
+    console.log("creando...", data);
+    try {
+        let response = await fetch(`${URL}/jedis`, {
+            method: "POST",
+            headers: {
+                "Content-type": 'application/json'
+            },
+            body: JSON.stringify(data)
+        });
+        if (response.ok) {
+            console.log('Elemento creado');
+            loadJedis();
+        } else {
+            throw new Error('Fallo al crear')
+        }
+    } catch (error) {
+        console.log("ERROR: ", error)
+    }
+});
+
+async function editJedi(id) {
+    let newName = prompt('Introduce el nuevo nombre');
+    let newSpecies = prompt('Introduce la nueva especie');
+    console.log("Nombre: %s, Especie: %s", newName, newSpecies);
+    let data = {
+        name: newName,
+        species: newSpecies
+    }
+    try {
+        let response = await fetch(`${URL}/jedis/${id}`, {
+            method: "PUT",
+            headers: {
+                "Content-type": 'application/json'
+            },
+            body: JSON.stringify(data)
+        });
+        if (response.ok) {
+            console.log('Elemento actualizado');
+            loadJedis();
+        } else {
+            throw new Error('Fallo al atualizar')
+        }
+    } catch (error) {
+        console.log("ERROR: ", error)
+    }
+}
+
+async function showJedi(id) {
+    try {
+        let response = await fetch(`${URL}/jedis/${id}`);
+        if (response.ok) {
+            let jedi = await response.json();
+            alert(jedi.name);
+        } else {
+            throw new Error('Fallo al motrar')
+        }
+    } catch (error) {
+        console.log("ERROR: ", error)
+    }
+}
+
+loadJedis();
